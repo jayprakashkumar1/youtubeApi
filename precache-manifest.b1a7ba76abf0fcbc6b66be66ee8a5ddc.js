abde4cdc58d5f81e879d0b7c39931a52f6251d2d
jayprakash Kumar <jptu143@gmail.com>
2019-01-23 01:06:28 +0530

    Updates

---
+++ precache-manifest.b1a7ba76abf0fcbc6b66be66ee8a5ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "160504fec34c73a5be5b592c6c6e361a",
+    "url": "/youtubeApi/static/media/qqq.160504fe.svg"
+  },
+  {
+    "revision": "c840774fb8b84f111c97301682524f9e",
+    "url": "/youtubeApi/static/media/filterIcon.c840774f.png"
+  },
+  {
+    "revision": "504147663460b9110625",
+    "url": "/youtubeApi/static/js/runtime~main.50414766.js"
+  },
+  {
+    "revision": "5d79846d4506e88d60c0",
+    "url": "/youtubeApi/static/js/main.5d79846d.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/youtubeApi/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "5d79846d4506e88d60c0",
+    "url": "/youtubeApi/static/css/main.80742016.chunk.css"
+  },
+  {
+    "revision": "c6f3b837ff32ed8c4c85041641799028",
+    "url": "/youtubeApi/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/youtubeApi/precache-manifest.b1a7ba76abf0fcbc6b66be66ee8a5ddc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/youtubeApi/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
